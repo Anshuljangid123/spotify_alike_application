a5971e46dbdd3a50b0630433d706208a4cfaca97
Anshul Jangid <anshuljan2003@gmail.com>
2025-07-12 19:55:36 +0530

    completed the project

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import {assets} from "../assets/assets"
+import { useNavigate } from 'react-router-dom'
 
 function Sidebar() {
+
+    const navigate = useNavigate();
   return (
     <div className='w-[25%] h-full flex-col gap-2 text-white hidden lg:flex' >
         <div className='bg-[#121212] h-[15%] rounded flex flex-col justify-around'>
-            <div className='flex items-center gap-3 pl-8 cursor-pointer'>
+            <div onClick={() =>navigate('/')} className='flex items-center gap-3 pl-8 cursor-pointer'>
                  <img className='w-6' src={assets.home_icon} alt=''/>
                  <p className='font-bold'> Home</p>
             </div>
@@ -42,7 +45,7 @@ function Sidebar() {
                     <p className='font-light'> we'll keep you update on new episodes</p>
                     <button className='px-4 py-1.5 bg-white text-[15px] text-black  rounded-full mt-4'> Browse Podcast</button>
                 </div>
-                
+
 
             </div>
 
